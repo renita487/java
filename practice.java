0b44bc5ea6b43f72ea2867dba05a74da29d1469d
Renita <annalanreni@gmail.com>
2026-07-19 22:52:32 +0530

    new files uploded

---
+++ practice.java
@@ -0,0 +1,25 @@
+// class practice{
+//     public static void main(String[] args){
+//         int a =15;
+//         int b=90;
+//         System.out.println(a+b);
+//         System.out.println("i need to develop a website for my project, can you help me with that?"); // i need to develop a website for my project, can you help me with that?
+//     }
+
+// }
+import java.util.*;
+class practice{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int button=sc.nextInt();
+        if(button==1){
+            System.out.println("Button 1");
+        }
+        else if(button==2){
+            System.out.println("Button 2");
+        }
+        else{
+            System.out.println("Button 3");
+        }
+    }
+}
